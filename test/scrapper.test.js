0f45dc45a31b94ade667a2fc81da15f723f1a356
FXHibon <hibonfx@gmail.com>
2016-03-11 19:27:22 +0100

    Test env

---
+++ test/scrapper.test.js
@@ -0,0 +1,15 @@
+/**
+ * Created by fx on 11/03/16.
+ */
+
+var should = require('should');
+var conf = require('../configuration.json');
+var scraper = require('../scrapper');
+
+describe('test', function () {
+    it('should works', function () {
+        should.exist({});
+    })
+});
+
+
